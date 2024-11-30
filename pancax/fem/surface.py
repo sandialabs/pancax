e825b017114a21695374b7e589788f95d9cec76a
Craig M. Hamel <cmhamel32@gmail.com>
2024-11-30 18:45:42 -0500

    adding surface stuff.

--- pancax/fem/surface.py
+++ pancax/fem/surface.py
@@ -1,6 +1,8 @@
+# from optimism.JaxConfig import *
 from jax import vmap
-import jax.numpy as np
+import jax.numpy as jnp
 
+# this is hard coded for tri elements
 
 def create_edges(coords, conns, edge_is_potentially_in_contact):
     edges = []
@@ -8,71 +10,53 @@ def create_edges(coords, conns, edge_is_potentially_in_contact):
         edgeCoords = coords.take(elem,0)
         for n in range(3):
             nn = (n+1)%3
-            edgeConn = np.array([n,nn])
+            edgeConn = jnp.array([n,nn])
             if (edge_is_potentially_in_contact(edgeCoords[edgeConn])):
                 edges.append([e,n])
-    return np.array(edges)
+    return jnp.array(edges)
 
 
-# def get_coords(mesh, side):
-#     elem = mesh.conns[side[0],:]
-#     lSide = side[1]
-#     elemCoords = mesh.coords.take(elem,0)
-#     return elemCoords[ np.array([lSide, (lSide+1)%3]), : ]
-
-def get_coords(coords, conns, side):
-    elem = conns[side[0],:]
+def get_coords(mesh, side):
+    elem = mesh.conns[side[0],:]
     lSide = side[1]
-    elemCoords = coords.take(elem,0)
-    return elemCoords[ np.array([lSide, (lSide+1)%3]), : ]
-
-
-def interpolate_nodal_field_on_edge(mesh, U, interpolationPoints, edge):
-    # This function isn't used yet.
-    # We may want to replicate parts of FunctionSpace to do surface integrals.
-    # 
-    fieldIndex = Surface.get_field_index(edge, mesh.conns)  
-    nodalValues = Surface.eval_field(U, fieldIndex)
-    return 0.0
+    elemCoords = mesh.coords.take(elem,0)
+    return elemCoords[ jnp.array([lSide, (lSide+1)%3]), : ]
 
 
 def integrate_values(quadratureRule, coords, gaussField):
-    xigauss, wgauss = quadratureRule
-    jac = np.linalg.norm(coords[0,:] - coords[1,:])
-    xgauss = np.array([coords[0] + (coords[1] - coords[0])*xi for xi in xigauss])
+    _, wgauss = quadratureRule
+    jac = jnp.linalg.norm(coords[0,:] - coords[1,:])
     dx = jac*wgauss
-    
-    res = dx.dot(gaussField)
-    return res
+    return dx.dot(gaussField)
 
 
 def integrate_function(quadratureRule, coords, field_func):
     xigauss, wgauss = quadratureRule
-    jac = np.linalg.norm(coords[0,:] - coords[1,:])
-    xgauss = np.array([coords[0] + (coords[1] - coords[0])*xi for xi in xigauss])
+    jac = jnp.linalg.norm(coords[0,:] - coords[1,:])
+    xgauss = jnp.array([coords[0] + (coords[1] - coords[0])*xi for xi in xigauss])
     dx = jac*wgauss
-    return np.dot(vmap(field_func)(xgauss), dx)
+    return jnp.dot(vmap(field_func)(xgauss), dx)
 
 
 def integrate_function_on_surface(quadratureRule, edges, mesh, func):
     F = vmap(integrate_function_on_edge, (None,0,None,None))
-    return np.sum(F(quadratureRule, edges, mesh, func))
+    return jnp.sum(F(quadratureRule, edges, mesh, func))
 
 
 def integrate_function_on_edge(quadratureRule, edge, mesh, func):
-    edgeCoords = get_coords(mesh.coords, mesh.conns, edge)
+    edgeCoords = get_coords(mesh, edge)
     xigauss, wgauss = quadratureRule
-    jac = np.linalg.norm(edgeCoords[0,:] - edgeCoords[1,:])
-    xgauss = edgeCoords[0] + np.outer(xigauss, edgeCoords[1] - edgeCoords[0])
+    jac = jnp.linalg.norm(edgeCoords[0,:] - edgeCoords[1,:])
+    xgauss = edgeCoords[0] + jnp.outer(xigauss, edgeCoords[1] - edgeCoords[0])
     dx = jac*wgauss
     normal = compute_normal(edgeCoords)
-    return np.dot(vmap(func, (0,None))(xgauss, normal), dx)
+    return jnp.dot(vmap(func, (0,None))(xgauss, normal), dx)
 
 
 def compute_normal(edgeCoords):
     tangent = edgeCoords[1]-edgeCoords[0]
-    normal = np.array([tangent[1], -tangent[0]])
-    return normal / np.linalg.norm(normal)
+    normal = jnp.array([tangent[1], -tangent[0]])
+    return normal / jnp.linalg.norm(normal)
 
 
 def get_field_index(edge, conns):
@@ -80,7 +64,7 @@ def get_field_index(edge, conns):
     elemConns = conns[elemIndex]
     n1 = edge[1]
     n2 = (n1+1)%3
-    return elemConns, np.array([n1,n2])
+    return elemConns, jnp.array([n1,n2])
 
 
 def eval_field(field, fieldIndex):
@@ -89,8 +73,6 @@ def eval_field(field, fieldIndex):
 
 def compute_edge_vectors(edgeCoords):
     tangent = edgeCoords[1]-edgeCoords[0]
-    normal = np.array([tangent[1], -tangent[0]])
-    jac = np.linalg.norm(tangent)
+    normal = jnp.array([tangent[1], -tangent[0]])
+    jac = jnp.linalg.norm(tangent)
     return tangent/jac, normal/jac, jac
-
-        
