c7fc79f18cf053f07980102466ec6746f0f79f66
Craig M. Hamel <cmhamel32@gmail.com>
2025-03-02 13:12:32 -0500

    adding hencky and log strain.

--- pancax/constitutive_models/__init__.py
+++ pancax/constitutive_models/__init__.py
@@ -4,5 +4,6 @@ from .properties import BoundedProperty, FixedProperty, Property
 # models
 from .mechanics.hyperelasticity.blatz_ko import BlatzKo
 from .mechanics.hyperelasticity.gent import Gent
+from .mechanics.hyperelasticity.hencky import Hencky
 from .mechanics.hyperelasticity.neohookean import NeoHookean
 from .mechanics.hyperelasticity.swanson import Swanson
---
+++ pancax/constitutive_models/mechanics/hyperelasticity/hencky.py
@@ -0,0 +1,21 @@
+from ..base import HyperelasticModel, Scalar, Tensor
+# TODO maybe move math/tensor_math into constiutive models?
+from ....math import tensor_math
+from ...properties import Property
+import jax.numpy as jnp
+
+
+class Hencky(HyperelasticModel):
+  bulk_modulus: Property
+  shear_modulus: Property
+
+  def energy(self, grad_u: Tensor) -> Scalar:
+    K, G = self.bulk_modulus, self.shear_modulus
+
+    # kinematics
+    E = self.log_strain(grad_u)
+    trE = jnp.trace(E)
+    psi = 0.5 * K * trE**2 + \
+          G * jnp.tensordot(E, E)
+          # G * tensor_math.norm_of_deviator_squared(E)
+    return psi
