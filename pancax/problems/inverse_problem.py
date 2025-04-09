96e6bce5d1e5ca17a1c55a034a24fc2e6b8404c5
Craig M. Hamel <chamel@sandia.gov>
2025-04-09 11:43:30 -0600

    linting for problems.

--- pancax/problems/inverse_problem.py
+++ pancax/problems/inverse_problem.py
@@ -7,19 +7,19 @@ from typing import Callable, List, Optional
 
 
 class InverseProblem(ForwardProblem):
-  field_data: FullFieldData
-  global_data: GlobalData
+    field_data: FullFieldData
+    global_data: GlobalData
 
-  def __init__(
-    self,
-    domain: BaseDomain,
-    physics: BasePhysics,
-    field_data: FullFieldData,
-    global_data: GlobalData,
-    ics: Optional[List[Callable]] = [],
-    dirichlet_bcs: Optional[List[DirichletBC]] = [],
-    neumann_bcs: Optional[List[NeumannBC]] = [],
-  ) -> None:
-    super().__init__(domain, physics, ics, dirichlet_bcs, neumann_bcs)
-    self.field_data = field_data
-    self.global_data = global_data
+    def __init__(
+        self,
+        domain: BaseDomain,
+        physics: BasePhysics,
+        field_data: FullFieldData,
+        global_data: GlobalData,
+        ics: Optional[List[Callable]] = [],
+        dirichlet_bcs: Optional[List[DirichletBC]] = [],
+        neumann_bcs: Optional[List[NeumannBC]] = [],
+    ) -> None:
+        super().__init__(domain, physics, ics, dirichlet_bcs, neumann_bcs)
+        self.field_data = field_data
+        self.global_data = global_data
