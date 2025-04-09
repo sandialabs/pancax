14828ba66df6d9cdefe89410dd8977013f5d498c
Craig M. Hamel <chamel@sandia.gov>
2025-04-09 10:37:26 -0600

    linting for optimizers.

--- pancax/optimizers/__init__.py
+++ pancax/optimizers/__init__.py
@@ -1,7 +1,6 @@
 from .adam import Adam
 from .base import Optimizer
-from .utils import *
 
-__all__ = [
-  "Adam"
-]
+# from .utils import *
+
+__all__ = ["Adam", "Optimizer"]
--- pancax/optimizers/utils.py
+++ pancax/optimizers/utils.py
@@ -4,41 +4,45 @@ import jax
 
 
 def trainable_filter(
-  params: any,
-  # freeze_fields: Optional[bool] = False,
-  freeze_properties: Optional[bool] = True,
-  freeze_basis: Optional[bool] = False,
-  # freeze_linear_layer: Optional[bool] = False
+    params: any,
+    # freeze_fields: Optional[bool] = False,
+    freeze_properties: Optional[bool] = True,
+    freeze_basis: Optional[bool] = False,
+    # freeze_linear_layer: Optional[bool] = False
 ):
-  # TODO there's some logic to work out here
-  filter_spec = jax.tree_util.tree_map(
-    lambda _: True, params
-  )
+    # TODO there's some logic to work out here
+    filter_spec = jax.tree_util.tree_map(lambda _: True, params)
 
-  # filter_spec = eqx.tree_at(
-  #   lambda tree: tree.fields,
-  #   filter_spec,
-  #   replace=not freeze_fields
-  # )
-  filter_spec = eqx.tree_at(
-    lambda tree: tree.properties.prop_params,
-    filter_spec,
-    replace=not freeze_properties
-  )
+    # filter_spec = eqx.tree_at(
+    #   lambda tree: tree.fields,
+    #   filter_spec,
+    #   replace=not freeze_fields
+    # )
+    filter_spec = eqx.tree_at(
+        lambda tree: tree.properties.prop_params,
+        filter_spec,
+        replace=not freeze_properties,
+    )
 
-  if freeze_basis:
-    try:
-      filter_spec = eqx.tree_at(
-        lambda tree: (tree.fields.basis.weight, tree.fields.basis.bias),
-        filter_spec,
-        replace=(not freeze_basis, not freeze_basis)
-      )
-      filter_spec = eqx.tree_at(
-        lambda tree: (tree.fields.linear.weight, tree.fields.linear.bias),
-        filter_spec,
-        replace=(True, True)
-      )
-    except:
-      raise AttributeError('This network does not have a basis!')
-  
-  return filter_spec
+    if freeze_basis:
+        try:
+            filter_spec = eqx.tree_at(
+                lambda tree: (
+                    tree.fields.basis.weight,
+                    tree.fields.basis.bias
+                ),
+                filter_spec,
+                replace=(not freeze_basis, not freeze_basis),
+            )
+            filter_spec = eqx.tree_at(
+                lambda tree: (
+                    tree.fields.linear.weight,
+                    tree.fields.linear.bias
+                ),
+                filter_spec,
+                replace=(True, True),
+            )
+        except AttributeError:
+            raise AttributeError("This network does not have a basis!")
+
+    return filter_spec
