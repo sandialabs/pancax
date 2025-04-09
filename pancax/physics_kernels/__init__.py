8d1b02cf0ef0c34d278392ed8f5ef24f005abaa3
Craig M. Hamel <chamel@sandia.gov>
2025-04-09 10:49:33 -0600

    linting for physics kernels.

--- pancax/physics_kernels/__init__.py
+++ pancax/physics_kernels/__init__.py
@@ -7,22 +7,27 @@ from .burgers_equation import BurgersEquation
 from .heat_equation import HeatEquation
 from .laplace_beltrami import LaplaceBeltrami
 from .poisson import Poisson
-from .solid_mechanics import BaseMechanicsFormulation, IncompressiblePlaneStress, PlaneStrain, ThreeDimensional
+from .solid_mechanics import (
+    BaseMechanicsFormulation,
+    IncompressiblePlaneStress,
+    PlaneStrain,
+    ThreeDimensional,
+)
 from .solid_mechanics import SolidMechanics
 
 __all__ = [
-  "BasePhysics",
-  "BaseEnergyFormPhysics",
-  "BaseStrongFormPhysics",
-  "BaseVariationalFormPhysics",
-  "BeerLambertLaw",
-  "BurgersEquation",
-  "HeatEquation",
-  "LaplaceBeltrami",
-  "Poisson",
-  "BaseMechanicsFormulation",
-  "IncompressiblePlaneStress",
-  "PlaneStrain",
-  "ThreeDimensional",
-  "SolidMechanics"
+    "BasePhysics",
+    "BaseEnergyFormPhysics",
+    "BaseStrongFormPhysics",
+    "BaseVariationalFormPhysics",
+    "BeerLambertLaw",
+    "BurgersEquation",
+    "HeatEquation",
+    "LaplaceBeltrami",
+    "Poisson",
+    "BaseMechanicsFormulation",
+    "IncompressiblePlaneStress",
+    "PlaneStrain",
+    "ThreeDimensional",
+    "SolidMechanics",
 ]
--- pancax/physics_kernels/beer_lambert_law.py
+++ pancax/physics_kernels/beer_lambert_law.py
@@ -10,21 +10,21 @@ Vector = Union[Float[Array, "2"], Float[Array, "3"]]
 
 
 class BeerLambertLaw(BaseStrongFormPhysics, BaseVariationalFormPhysics):
-  field_value_names: tuple[int, ...]
-  d: Vector = eqx.field(static=True)
-  sigma: Property
-  
-  def __init__(self, d: Vector, sigma: Property):
-    super().__init__(('I',))
-    self.d = d
-    self.sigma = sigma
+    field_value_names: tuple[int, ...]
+    d: Vector = eqx.field(static=True)
+    sigma: Property
 
-  def residual(self, params, x, t, u, v, grad_u, grad_v, *args):
-    # note this is not stabilized
-    eq = jnp.dot(self.d, grad_u.T) + self.sigma * u
-    return v * eq
-    
-  def strong_form_residual(self, params, x, t, *args):
-    I = self.field_values(params, x, t, *args)
-    grad_I = self.field_gradients(params, x, t, *args)
-    return jnp.dot(self.d, grad_I.T) + self.sigma * I
+    def __init__(self, d: Vector, sigma: Property):
+        super().__init__(("I",))
+        self.d = d
+        self.sigma = sigma
+
+    def residual(self, params, x, t, u, v, grad_u, grad_v, *args):
+        # note this is not stabilized
+        eq = jnp.dot(self.d, grad_u.T) + self.sigma * u
+        return v * eq
+
+    def strong_form_residual(self, params, x, t, *args):
+        val_I = self.field_values(params, x, t, *args)
+        grad_I = self.field_gradients(params, x, t, *args)
+        return jnp.dot(self.d, grad_I.T) + self.sigma * val_I
