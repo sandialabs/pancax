d7ca0bc617cc60c8e4fd6d43b224617a27d015dd
Craig M. Hamel <cmhamel32@gmail.com>
2025-08-11 22:09:52 -0400

    fixing linter error

--- pancax/utils.py
+++ pancax/utils.py
@@ -16,13 +16,13 @@ def find_data_file(data_file_in: str):
     call_file_dir = call_file.parent
 
     data_file = Path(os.path.join(call_file_dir, data_file_in))
-    
+
     if data_file.is_file():
         print(f"Found {data_file_in} in {data_file.parent}")
         return data_file
 
     data_file = Path(os.path.join(call_file_dir, "data", data_file_in))
-    
+
     if data_file.is_file():
         print(f"Found {data_file_in} in {data_file.parent}")
         return data_file
@@ -42,13 +42,13 @@ def find_mesh_file(mesh_file_in: str):
     if mesh_file.is_file():
         print(f"Found {mesh_file_in} in {mesh_file.parent}")
         return mesh_file
-    
+
     mesh_file = Path(os.path.join(call_file, "mesh", mesh_file_in))
 
     if mesh_file.is_file():
         print(f"Found {mesh_file_in} in {mesh_file.parent}")
         return mesh_file
-    
+
     raise MeshFileNotFoundException(
         f"Could not find data file {mesh_file_in} in either "
         f"{call_file_dir} or {call_file_dir}/mesh"
